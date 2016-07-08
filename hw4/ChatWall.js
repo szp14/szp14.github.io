63f80693a78b4b91da53ea4f03792e6958b5823e
szp14 <szp_14@163.com>
2016-07-08 22:09:21 +0800

    chatWallFinal2

--- hw4/ChatWall.js
+++ hw4/ChatWall.js
@@ -1,4 +1,5 @@
 var eventQueue = [];
+var protectQueue = [];
 var timer;
 var isProtected = false;
 var originBar;
@@ -9,19 +10,21 @@ function copyTo(bar1, bar2)
 {
 	if(bar1.children[0].children[0].innerText != 'admin')
 	{
+		debugger;
 		bar2.children[0].children[2].src = bar1.children[0].children[2].src;
 		bar2.children[0].children[0].innerText = bar1.children[0].children[0].innerText;
 		bar2.children[1].innerHTML = bar1.children[1].innerHTML;
 	}
 }
 
-function protectFirstBar(ok)
+function protectFirstBar()
 {
-	var tempQ = eventQueue;
-	eventQueue.length = 0;
+	if(isRunning) return;
+	isProtected = !isProtected;
+	protectQueue.pop();
 	var parent = block.parentNode;  //chatFixedBlock
 	var grandp = parent.parentNode; //mainWindow
-	if(ok == true)
+	if(isProtected == true)
 	{
 		var firstBar = block.children[0];
 		var secondBar = block.children[1];
@@ -31,6 +34,8 @@ function protectFirstBar(ok)
 		secondBar.style.height = '50%';
 		thirdBar.style.height = '50%';
 		parent.style.height = '53%';
+
+		copyTo(firstBar, originBar);
 		firstBar.children[0].children[2].src = 'icon.jpg';
 		firstBar.children[0].children[0].innerText = 'admin';
 
@@ -58,7 +63,6 @@ function protectFirstBar(ok)
 		block.appendChild(secondBar);
 		block.appendChild(thirdBar);
 	}
-	eventQueue = tempQ;
 }
 
 blockListener = function()
@@ -71,9 +75,14 @@ blockListener = function()
 	this.classList.remove('animaMoveUp');
 	this.offsetWidth = this.offsetWidth;
 	this.classList.add('animaMoveUp');
+	isRunning = false;
 	if(eventQueue.length > 0)
 	{
 		updateInfo();
+	}
+	if(protectQueue.length > 0)
+	{
+		protectFirstBar(isProtected = !isProtected);
 	}
 }
 
@@ -155,7 +164,8 @@ function adminInfo(message)
 { 
 	if(isProtected == false)
 	{
-		protectFirstBar(isProtected = !isProtected);
+		protectQueue.push(1);
+		protectFirstBar(isProtected);
 	}
 	clearTimeout(timer);
 	var info = block.parentNode.parentNode.children[1].children[1];
@@ -163,7 +173,8 @@ function adminInfo(message)
 	info.innerHTML = "<marquee scrollamount = '50' style = 'color: red'>" + message.content + "</marquee>";
 	timer = setTimeout(function()
 	{
-		protectFirstBar(isProtected = !isProtected);
+		protectQueue.push(1);
+		protectFirstBar(isProtected);
 	}, 10000);
 }
 
@@ -184,6 +195,7 @@ function updateInfo()
 	newBar.style.height = isProtected ? '50%' : '33%';
 	block.appendChild(newBar);
 	block.addEventListener('webkitAnimationEnd', blockListener, false);
+	isRunning = true;
 	block.style.animationPlayState = 'running';	
 }
 
